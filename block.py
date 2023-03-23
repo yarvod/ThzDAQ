1143a8ac5f654e855af8249c97980f77d9078494
Snake <yar.vod.ole@gmail.com>
2023-03-23 20:17:33 +0300

    simple funcs

--- block.py
+++ block.py
@@ -4,7 +4,16 @@ from collections import defaultdict
 from config import BLOCK_ADDRESS, BLOCK_PORT
 
 
-class Block:
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Block(metaclass=Singleton):
 
     def __init__(self, host: str, port: int, bias_dev: str = 'DEV4', ctrl_dev: str = 'DEV3'):
         self.host = host
@@ -13,29 +22,37 @@ class Block:
         self.ctrl_dev = ctrl_dev
         self.iv = defaultdict(list)
 
-    def manipulate(self, cmd: str):
+    def manipulate(self, cmd: str, s: socket.socket):
         if type(cmd) != bytes:
             cmd = bytes(cmd, 'utf-8')
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.host, self.port))
-            s.sendall(cmd)
-            data = s.recv(1024)
+        s.connect((self.host, self.port))
+        s.sendall(cmd)
+        data = s.recv(1024)
         return data.decode().rstrip()
 
     def set_ctrl_current(self, curr: float):
-        return self.manipulate(f'CTRL:{self.ctrl_dev}:CURR {curr}')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return self.manipulate(f'CTRL:{self.ctrl_dev}:CURR {curr}', s)
 
     def get_ctrl_current(self):
-        return self.manipulate(f'CTRL:{self.ctrl_dev}:CURR?')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return self.manipulate(f'CTRL:{self.ctrl_dev}:CURR?', s)
 
     def get_current(self):
-        return self.manipulate(f'BIAS:{self.bias_dev}:CURR?')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return self.manipulate(f'BIAS:{self.bias_dev}:CURR?', s)
 
     def get_voltage(self):
-        return self.manipulate(f'BIAS:{self.bias_dev}:VOLT?')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return self.manipulate(f'BIAS:{self.bias_dev}:VOLT?', s)
 
     def set_voltage(self, volt: float):
-        return self.manipulate(f'BIAS:{self.bias_dev}:VOLT {volt}')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return self.manipulate(f'BIAS:{self.bias_dev}:VOLT {volt}', s)
+
+    # def scan_cl_current(self, volt, cl_i_from, sis_i_from):
+    #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #
 
 
 if __name__ == '__main__':
--- ui.py
+++ ui.py
@@ -1,6 +1,35 @@
 from PyQt5.QtWidgets import *
 import sys
 
+from block import Block
+from config import BLOCK_ADDRESS, BLOCK_PORT
+
+
+def update_block(block_ip, block_port):
+    block = Block(block_ip, block_port)
+    block.host = block_ip
+    block.port = int(block_port)
+
+
+class UtilsMixin:
+
+    @property
+    def block(self):
+        return Block()
+
+    def set_voltage(self):
+        self.block.set_voltage(float(self.voltage_s.text()))
+        current = self.block.get_current()
+        self.current_g.setText(current)
+        voltage = self.block.get_voltage()
+        self.voltage_g.setText(voltage)
+
+    def get_voltage_current(self):
+        current = self.block.get_current()
+        self.current_g.setText(current)
+        voltage = self.block.get_voltage()
+        self.voltage_g.setText(voltage)
+
 
 class App(QMainWindow):
 
@@ -9,8 +38,8 @@ class App(QMainWindow):
         self.title = 'CL manager'
         self.left = 0
         self.top = 0
-        self.width = 300
-        self.height = 200
+        self.width = 400
+        self.height = 300
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
@@ -27,9 +56,9 @@ class TabsWidget(QWidget):
         self.layout = QVBoxLayout(self)
 
         # Initialize tab screen
-        self.tabs = QTabWidget()
-        self.tab_setup = SetupTabWidget()
-        self.tab_block = QWidget()
+        self.tabs = QTabWidget(self)
+        self.tab_setup = SetUpTabWidget(self)
+        self.tab_block = BlockTabWidget(self)
         self.tabs.resize(300, 200)
 
         # Add tabs
@@ -41,7 +70,7 @@ class TabsWidget(QWidget):
         self.setLayout(self.layout)
 
 
-class SetupTabWidget(QWidget):
+class SetUpTabWidget(QWidget):
 
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
@@ -51,13 +80,61 @@ class SetupTabWidget(QWidget):
         self.blockIPLabel = QLabel(self)
         self.blockIPLabel.setText('Block IP:')
         self.block_ip = QLineEdit(self)
+        self.block_ip.setText(BLOCK_ADDRESS)
 
         self.block_ip.move(80, 20)
         self.block_ip.resize(200, 32)
         self.blockIPLabel.move(20, 20)
 
-        self.pushButton1 = QPushButton("PyQt5 button")
-        self.layout.addWidget(self.pushButton1)
+        self.blockPortLabel = QLabel(self)
+        self.blockPortLabel.setText('Block Port:')
+        self.block_port = QLineEdit(self)
+        self.block_port.setText(BLOCK_PORT)
+
+        self.block_port.move(80, 60)
+        self.block_port.resize(200, 32)
+        self.blockPortLabel.move(20, 60)
+
+        update_block(self.block_ip.text(), self.block_port.text())
+
+        self.btn_update = QPushButton("Update")
+        self.layout.addWidget(self.btn_update)
+        self.btn_update.clicked.connect(
+            lambda: update_block(self.block_ip.text(), self.block_port.text())
+        )
+
+
+class BlockTabWidget(QWidget, UtilsMixin):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
+
+        self.voltGLabel = QLabel(self)
+        self.voltGLabel.setText('V Bias, V:')
+        self.voltGLabel.move(20, 20)
+        self.voltage_g = QLabel(self)
+        self.voltage_g.move(80, 20)
+
+        self.currGLabel = QLabel(self)
+        self.currGLabel.setText('I, A:')
+        self.currGLabel.move(160, 20)
+        self.current_g = QLabel(self)
+        self.current_g.move(200, 20)
+
+        self.voltGLabel = QLabel(self)
+        self.voltGLabel.setText('V Bias, V:')
+        self.voltGLabel.move(20, 80)
+        self.voltage_s = QLineEdit(self)
+        self.voltage_s.move(80, 80)
+        self.voltage_s.resize(100, 32)
+
+        self.btn_set_voltage = QPushButton("Set Voltage")
+        self.btn_set_voltage.clicked.connect(lambda: self.set_voltage())
+        self.layout.addWidget(self.btn_set_voltage)
+
+        self.get_voltage_current()
 
 
 if __name__ == '__main__':
