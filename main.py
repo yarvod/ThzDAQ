e65f7f7641dc282f5b0ebc32c7c11c5f673e7795
yarvod <yar.vod.ole@gmail.com>
2023-03-26 15:40:56 +0300

    move to main

--- main.py
+++ main.py
@@ -1,85 +1,143 @@
-from tkinter import *
-from tkinter.ttk import Notebook
+from PyQt6.QtWidgets import *
+import sys
 
+from block import Block
 from config import BLOCK_ADDRESS, BLOCK_PORT
 
 
-class BaseMixin:
-    ...
+def update_block(block_ip, block_port):
+    block = Block(block_ip, block_port)
+    block.host = block_ip
+    block.port = int(block_port)
 
 
-class UI(Frame, BaseMixin):
+class UtilsMixin:
 
-    def __init__(self, isapp=True, name='control line manager'):
-        Frame.__init__(self, name=name)
-        self.pack(expand=Y, fill=BOTH)
-        self.master.title('Control Line manager')
-        self.isapp = isapp
-        self._create_widgets()
+    @property
+    def block(self):
+        return Block()
 
-    def _create_widgets(self):
-        self._create_demo_panel()
+    def set_voltage(self):
+        self.block.set_voltage(float(self.voltage_s.text()))
+        current = self.block.get_current()
+        self.current_g.setText(current)
+        voltage = self.block.get_voltage()
+        self.voltage_g.setText(voltage)
 
-    def _create_demo_panel(self):
-        demo_panel = Frame(self, name='demo')
-        demo_panel.pack(side=TOP, fill=BOTH, expand=Y)
-        nb = Notebook(demo_panel, name='notebook')
-        nb.enable_traversal()
-        nb.pack(fill=BOTH, expand=Y, padx=2, pady=2)
-        self._create_setup_tab(nb)
-        self._create_block_tab(nb)
-
-    def _create_setup_tab(self, nb):
-        frame = Frame(nb, name='setup')
-        self.block_address = StringVar(value=BLOCK_ADDRESS)
-        self.block_port = StringVar(value=BLOCK_PORT)
-
-        Label(frame, text='Block IP', font=('bold', '14')).grid(row=0, column=0, padx=5, pady=5, sticky=W)
-        Entry(frame, textvariable=self.block_address).grid(row=0, column=1, padx=5, pady=5, sticky=W)
-        Label(frame, text='Block PORT', font=('bold', '14')).grid(row=1, column=0, padx=5, pady=5, sticky=W)
-        Entry(frame, textvariable=self.block_port).grid(row=1, column=1, padx=5, pady=5, sticky=W)
-
-        frame.rowconfigure(1, weight=1)
-        frame.columnconfigure((0, 1), weight=1, uniform=1)
-
-        nb.add(frame, text='SetUp', underline=0, padding=2)
-
-    def _create_block_tab(self, nb):
-        frame = Frame(nb, name='sis block')
-
-        block_frame = Frame(frame)
-        block_frame.pack()
-
-        self.iv_point_num = StringVar()
-        Label(block_frame, text='Point num:').grid(row=0, column=0, padx=5, pady=5)
-        Entry(block_frame, textvariable=self.iv_point_num).grid(row=0, column=1, padx=5, pady=5)
-
-        self.bias_volt = StringVar()
-        Label(block_frame, text='Bias Volt, V:').grid(row=0, column=2, padx=5, pady=5)
-        Entry(block_frame, textvariable=self.bias_volt).grid(row=0, column=3, padx=5, pady=5)
-
-        self.bias_curr = StringVar()
-        Label(block_frame, text='Bias Curr, A').grid(row=0, column=4, padx=5, pady=5)
-        Entry(block_frame, textvariable=self.bias_curr).grid(row=0, column=5, padx=5, pady=5)
-
-        Button(block_frame, text='Measure curve', command=lambda: self.meas_iv_new(save=True)) \
-            .grid(row=1, column=0, padx=5, pady=5)
+    def get_voltage_current(self):
+        current = self.block.get_current()
+        self.current_g.setText(current)
+        voltage = self.block.get_voltage()
+        self.voltage_g.setText(voltage)
 
 
-        self.use_offset = BooleanVar()
-        Checkbutton(block_frame, text='Use offset', var=self.use_offset).grid(row=2, column=0, padx=5, pady=5)
+class App(QMainWindow):
 
-        Button(block_frame, text='Calculate offset', command=lambda: self.calc_offset()) \
-            .grid(row=2, column=1, padx=5, pady=5)
+    def __init__(self):
+        super().__init__()
+        self.title = 'CL manager'
+        self.left = 0
+        self.top = 0
+        self.width = 400
+        self.height = 300
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-        Button(block_frame, text='set 0', command=lambda: self.niblock.set_zero()) \
-            .grid(row=3, column=0, padx=5, pady=5)
+        self.table_widget = TabsWidget(self)
+        self.setCentralWidget(self.table_widget)
 
-        Button(block_frame, text='update params', command=lambda: self.niblock.update_params()) \
-            .grid(row=3, column=1, padx=5, pady=5)
+        self.show()
 
-        nb.add(frame, text='SIS block', underline=0, padding=2)
+
+class TabsWidget(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget(self)
+        self.tab_setup = SetUpTabWidget(self)
+        self.tab_block = BlockTabWidget(self)
+        self.tabs.resize(300, 200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab_setup, "Set Up")
+        self.tabs.addTab(self.tab_block, "SIS Block")
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+
+class SetUpTabWidget(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
+
+        self.blockIPLabel = QLabel(self)
+        self.blockIPLabel.setText('Block IP:')
+        self.block_ip = QLineEdit(self)
+        self.block_ip.setText(BLOCK_ADDRESS)
+
+        self.block_ip.move(80, 20)
+        self.block_ip.resize(200, 32)
+        self.blockIPLabel.move(20, 20)
+
+        self.blockPortLabel = QLabel(self)
+        self.blockPortLabel.setText('Block Port:')
+        self.block_port = QLineEdit(self)
+        self.block_port.setText(BLOCK_PORT)
+
+        self.block_port.move(80, 60)
+        self.block_port.resize(200, 32)
+        self.blockPortLabel.move(20, 60)
+
+        # update_block(self.block_ip.text(), self.block_port.text())
+
+        self.btn_update = QPushButton("Update")
+        self.layout.addWidget(self.btn_update)
+        self.btn_update.clicked.connect(
+            lambda: update_block(self.block_ip.text(), self.block_port.text())
+        )
+
+
+class BlockTabWidget(QWidget, UtilsMixin):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
+
+        self.voltGLabel = QLabel(self)
+        self.voltGLabel.setText('V Bias, V:')
+        self.voltGLabel.move(20, 20)
+        self.voltage_g = QLabel(self)
+        self.voltage_g.move(80, 20)
+
+        self.currGLabel = QLabel(self)
+        self.currGLabel.setText('I, A:')
+        self.currGLabel.move(160, 20)
+        self.current_g = QLabel(self)
+        self.current_g.move(200, 20)
+
+        self.voltGLabel = QLabel(self)
+        self.voltGLabel.setText('V Bias, V:')
+        self.voltGLabel.move(20, 80)
+        self.voltage_s = QDoubleSpinBox(self)
+        self.voltage_s.move(80, 80)
+        self.voltage_s.resize(100, 32)
+
+        self.btn_set_voltage = QPushButton("Set Voltage")
+        self.btn_set_voltage.clicked.connect(lambda: self.set_voltage())
+        self.layout.addWidget(self.btn_set_voltage)
+
+        # self.get_voltage_current()
 
 
 if __name__ == '__main__':
-    UI().mainloop()
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec())
