8c54c7f24f4448e79450df0a8f33db63cb5fbfcc
yarvod <yar.vod.ole@gmail.com>
2023-04-03 16:50:23 +0300

    deal with graph clearing

--- ui/windows/graphWindow.py
+++ ui/windows/graphWindow.py
@@ -1,7 +1,8 @@
 import logging
+from collections import defaultdict
 from typing import Iterable
 
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton
 import pyqtgraph as pg
 
 
@@ -18,9 +19,12 @@ class GraphWindow(QWidget):
         super().__init__()
         self.setWindowTitle(self.window_title)
         layout = QVBoxLayout()
-        self.datasets = []
         self.graphWidget = pg.PlotWidget()
+        self.btnRemoveGraphs = QPushButton("Remove hidden graphs")
+        self.btnRemoveGraphs.clicked.connect(self.remove_hidden_graphs)
+        layout.addWidget(self.btnRemoveGraphs)
         layout.addWidget(self.graphWidget)
+        self.datasets = defaultdict(dict)
         self.prepare()
         self.plotNew(x, y)
         self.setLayout(layout)
@@ -34,26 +38,36 @@ class GraphWindow(QWidget):
         self.graphWidget.addLegend()
         self.graphWidget.showGrid(x=True, y=True)
 
-    def plotGraph(self, index: int = 0):
-        pen = pg.mkPen(color="#303036")
+    def plotGraph(self, ds_id: int = 1):
+        pen = pg.mkPen(color=pg.intColor(ds_id * 10, 100))
         try:
-            x = self.datasets[index]["x"]
-            y = self.datasets[index]["y"]
+            x = self.datasets[ds_id]["x"]
+            y = self.datasets[ds_id]["y"]
         except (IndexError, KeyError) as e:
             logger.error(f"Plot graph Error: {e}")
             return
         self.graphWidget.plot(
-            x,
-            y,
-            name=f"{index + 1}",
-            pen=pen,
-            symbolSize=10,
+            x, y, name=f"{ds_id}", pen=pen, symbolSize=5, symbolBrush=pen.color()
         )
 
     def addData(self, x: Iterable, y: Iterable) -> int:
-        self.datasets.append({"x": x, "y": y})
-        return len(self.datasets) - 1
+        ds_id = max(self.datasets.keys(), default=0) + 1
+        self.datasets[ds_id] = {"x": x, "y": y}
+        return ds_id
 
     def plotNew(self, x: Iterable, y: Iterable):
-        index = self.addData(x, y)
-        self.plotGraph(index)
+        ds_id = self.addData(x, y)
+        self.plotGraph(ds_id)
+
+    def remove_hidden_graphs(self):
+        plotItem = self.graphWidget.getPlotItem()
+        items_to_remove = {
+            int(item.name()): item for item in plotItem.items if not item.isVisible()
+        }
+        for item in items_to_remove.values():
+            plotItem.removeItem(item)
+        self.datasets = {
+            ds_id: ds
+            for ds_id, ds in self.datasets.items()
+            if ds_id not in items_to_remove.keys()
+        }
