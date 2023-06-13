5adfc60284bda8c0e505381711ba4ca5fead7371
yarvod <yar.vod.ole@gmail.com>
2023-06-13 17:44:07 +0300

    modify build bat

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-pyInstaller main.py -n SIS_manager --onedir
+pyInstaller main.py -n SIS_manager --onedir --icon=.\assets\logo.png --noconsole -y
+mkdir .\dist\SIS_manager\assets
+copy .\assets\* .\dist\SIS_manager\assets\
