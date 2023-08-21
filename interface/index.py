e57b91fe2cc3eca1acfcd86b52aec957f913332c
Snake <yar.vod.ole@gmail.com>
2023-08-21 15:43:01 +0300

    prettify - add style

--- interface/index.py
+++ interface/index.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6 import QtGui
 
+from interface import style
 from interface.views.index import TabsWidget
 
 
@@ -10,9 +11,10 @@ class App(QMainWindow):
         self.title = "SIS manager"
         self.left = 0
         self.top = 0
-        self.width = 400
+        self.width = 450
         self.height = 700
         self.setWindowIcon(QtGui.QIcon("./assets/logo_small.png"))
+        self.setStyleSheet(style.GLOBAL_STYLE)
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
