ac8dc4e7c320d909b894bed252e2d7b4bf699f58
Snake <yar.vod.ole@gmail.com>
2023-08-23 14:11:57 +0300

    temperature - add plot monitor

--- interface/views/temperatureControllerTabWidget.py
+++ interface/views/temperatureControllerTabWidget.py
@@ -1,5 +1,5 @@
 import time
-from typing import Tuple
+from typing import Tuple, Dict
 
 from PyQt6.QtCore import QThread, pyqtSignal
 from PyQt6.QtWidgets import (
@@ -10,22 +10,35 @@ from PyQt6.QtWidgets import (
     QLabel,
     QPushButton,
     QSizePolicy,
+    QCheckBox,
 )
 
 from api.LakeShore.temperature_controller import TemperatureController
+from interface.components.DoubleSpinBox import DoubleSpinBox
+from interface.windows.temperatureGraphWindow import TemperatureGraphWindow
 from store.state import state
 
 
 class MonitorThread(QThread):
-    temperatures = pyqtSignal(tuple)
+    temperatures = pyqtSignal(dict)
 
     def run(self):
         tc = TemperatureController(host=state.LAKE_SHORE_IP, port=state.LAKE_SHORE_PORT)
+        start_time = time.time()
+        i = 0
         while 1:
             temp_a = tc.get_temperature_a()
             temp_c = tc.get_temperature_c()
-            self.temperatures.emit((temp_a, temp_c))
-            time.sleep(0.2)
+            self.temperatures.emit(
+                {
+                    "temp_a": temp_a,
+                    "temp_c": temp_c,
+                    "time": time.time() - start_time,
+                    "reset": i == 0,
+                }
+            )
+            time.sleep(state.LAKE_SHORE_STREAM_STEP_DELAY)
+            i += 1
 
 
 class TemperatureControllerTabWidget(QWidget):
@@ -33,6 +46,7 @@ class TemperatureControllerTabWidget(QWidget):
         super().__init__(*args, **kwargs)
         self.layout = QVBoxLayout(self)
         self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.temperatureStreamGraphWindow = None
 
         self.createGroupMonitor()
 
@@ -58,6 +72,22 @@ class TemperatureControllerTabWidget(QWidget):
         self.tempC = QLabel(self)
         self.tempC.setText("0")
 
+        self.temperatureStreamStepDelayLabel = QLabel(self)
+        self.temperatureStreamStepDelayLabel.setText("Step delay, s")
+        self.temperatureStreamStepDelay = DoubleSpinBox(self)
+        self.temperatureStreamStepDelay.setRange(0.01, 1)
+        self.temperatureStreamStepDelay.setValue(state.LAKE_SHORE_STREAM_STEP_DELAY)
+
+        self.checkTemperatureStreamPlot = QCheckBox(self)
+        self.checkTemperatureStreamPlot.setText("Plot stream time line")
+
+        self.temperatureStreamTimeLabel = QLabel(self)
+        self.temperatureStreamTimeLabel.setText("Time window, s")
+        self.temperatureStreamTime = DoubleSpinBox(self)
+        self.temperatureStreamTime.setDecimals(0)
+        self.temperatureStreamTime.setRange(10, 43200)
+        self.temperatureStreamTime.setValue(state.LAKE_SHORE_STREAM_TIME)
+
         self.btnStartMonitor = QPushButton("Start")
         self.btnStartMonitor.clicked.connect(self.startMonitor)
 
@@ -69,13 +99,21 @@ class TemperatureControllerTabWidget(QWidget):
         layout.addWidget(self.tempCLabel, 0, 1)
         layout.addWidget(self.tempA, 1, 0)
         layout.addWidget(self.tempC, 1, 1)
-        layout.addWidget(self.btnStartMonitor, 2, 0)
-        layout.addWidget(self.btnStopMonitor, 2, 1)
+        layout.addWidget(self.temperatureStreamStepDelayLabel, 2, 0)
+        layout.addWidget(self.temperatureStreamStepDelay, 2, 1)
+        layout.addWidget(self.checkTemperatureStreamPlot, 3, 0)
+        layout.addWidget(self.temperatureStreamTimeLabel, 4, 0)
+        layout.addWidget(self.temperatureStreamTime, 4, 1)
+        layout.addWidget(self.btnStartMonitor, 5, 0)
+        layout.addWidget(self.btnStopMonitor, 5, 1)
 
         self.groupMonitor.setLayout(layout)
 
     def startMonitor(self):
         self.thread_monitor = MonitorThread()
+        state.LAKE_SHORE_STREAM_TIME = self.temperatureStreamTime.value()
+        state.LAKE_SHORE_STREAM_PLOT_GRAPH = self.checkTemperatureStreamPlot.isChecked()
+        state.LAKE_SHORE_STREAM_STEP_DELAY = self.temperatureStreamStepDelay.value()
         self.thread_monitor.start()
         self.btnStartMonitor.setEnabled(False)
         self.btnStopMonitor.setEnabled(True)
@@ -90,7 +128,25 @@ class TemperatureControllerTabWidget(QWidget):
     def stopMonitor(self):
         self.thread_monitor.terminate()
 
-    def updateMonitor(self, temperatures: Tuple[float, float]):
-        temp_a, temp_c = temperatures
-        self.tempA.setText(f"{temp_a}")
-        self.tempC.setText(f"{temp_c}")
+    def updateMonitor(self, measure: Dict):
+        self.tempA.setText(f"{measure.get('temp_a')}")
+        self.tempC.setText(f"{measure.get('temp_c')}")
+        if state.LAKE_SHORE_STREAM_PLOT_GRAPH:
+            self.show_temperature_stream_graph(measure=measure)
+
+    def show_temperature_stream_graph(self, measure: Dict):
+        if self.temperatureStreamGraphWindow is None:
+            self.temperatureStreamGraphWindow = TemperatureGraphWindow()
+        self.temperatureStreamGraphWindow.plotNew(
+            ds_id="A",
+            x=measure.get("time"),
+            y=measure.get("temp_a"),
+            reset_data=measure.get("reset"),
+        )
+        self.temperatureStreamGraphWindow.plotNew(
+            ds_id="C",
+            x=measure.get("time"),
+            y=measure.get("temp_c"),
+            reset_data=measure.get("reset"),
+        )
+        self.temperatureStreamGraphWindow.show()
---
+++ interface/windows/temperatureGraphWindow.py
@@ -0,0 +1,76 @@
+import logging
+from collections import defaultdict
+
+from PyQt6.QtWidgets import QWidget, QVBoxLayout
+import pyqtgraph as pg
+
+from store.state import state
+
+logger = logging.getLogger(__name__)
+
+
+class TemperatureGraphWindow(QWidget):
+    window_title = "Temperature graphs"
+    graph_title = "Temperature (time)"
+    y_label = "Temperature, K"
+    x_label = "Time, s"
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(self.window_title)
+        layout = QVBoxLayout()
+        self.graphWidget = pg.PlotWidget()
+        layout.addWidget(self.graphWidget)
+        self.dataset = defaultdict(dict[str, list])
+        self.prepare()
+        self.setLayout(layout)
+
+    def get_color(self, ds_id: str):
+        if ds_id == "A":
+            return pg.intColor(1 * 10, 100)
+        elif ds_id == "C":
+            return pg.intColor(7 * 10, 100)
+
+    def prepare(self) -> None:
+        self.graphWidget.setBackground("w")
+        self.graphWidget.setTitle(self.graph_title, color="#413C58", size="20pt")
+        styles = {"color": "#413C58", "font-size": "15px"}
+        self.graphWidget.setLabel("left", self.y_label, **styles)
+        self.graphWidget.setLabel("bottom", self.x_label, **styles)
+        self.graphWidget.addLegend()
+        self.graphWidget.showGrid(x=True, y=True)
+
+    def plotGraph(self, ds_id: str) -> None:
+        try:
+            x = self.dataset[ds_id]["x"]
+            y = self.dataset[ds_id]["y"]
+        except (IndexError, KeyError) as e:
+            logger.error(f"Plot graph Error: {e}")
+            return
+
+        plotItem = self.graphWidget.getPlotItem()
+        items = {item.name(): item for item in plotItem.items}
+        if items.get(ds_id):
+            items.get(ds_id).setData(x, y)
+            return
+        pen = pg.mkPen(color=self.get_color(ds_id), width=2)
+        self.graphWidget.plot(
+            x, y, name=ds_id, pen=pen, symbolSize=6, symbolBrush=pen.color()
+        )
+
+    def addData(self, ds_id: str, x: float, y: float, reset_data: bool = True) -> None:
+        if reset_data:
+            self.dataset[ds_id]["y"] = [y]
+            self.dataset[ds_id]["x"] = [x]
+            return
+
+        if max(self.dataset[ds_id]["x"]) >= state.LAKE_SHORE_STREAM_TIME:
+            del self.dataset[ds_id]["y"][0]
+            del self.dataset[ds_id]["x"][0]
+
+        self.dataset[ds_id]["y"].append(y)
+        self.dataset[ds_id]["x"].append(x)
+
+    def plotNew(self, ds_id: str, x: float, y: float, reset_data: bool = True) -> None:
+        self.addData(ds_id=ds_id, x=x, y=y, reset_data=reset_data)
+        self.plotGraph(ds_id)
