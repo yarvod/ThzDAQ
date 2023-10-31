c0b53e5cc877ba844285879e678fd6ff2c5aa76d
Snake <yar.vod.ole@gmail.com>
2023-10-31 14:20:10 +0300

    ads - add logo 2.0

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ if "%1"=="" (
     set "app_name=%1"
 )
 
-pyInstaller main.py -n %app_name% --onedir --icon=".\assets\logo.png" --noconsole --windowed -y
+pyInstaller main.py -n %app_name% --onedir --icon=".\assets\logo2.png" --noconsole --windowed -y
 mkdir .\dist\%app_name%\assets
 copy .\assets\* .\dist\%app_name%\assets\
 
