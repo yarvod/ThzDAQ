04b7bb7a89f5418be408de3c797a2e42966ce05d
Snake <yar.vod.ole@gmail.com>
2023-12-11 18:31:05 +0300

    master - rollback windows building

--- build.bat
+++ build.bat
@@ -7,6 +7,9 @@ if "%1"=="" (
     set "app_name=%1"
 )
 
-pyInstaller main.py -n %app_name% --onedir --icon=".\assets\logo2.png" --noconsole --windowed -y --add-data="assets:assets" --add-data="settings.ini:."
+pyInstaller main.py -n %app_name% --onedir --icon=".\assets\logo2.png" --noconsole --windowed -y
+mkdir .\dist\%app_name%\assets
+copy .\assets\* .\dist\%app_name%\assets\
+copy .\settings.ini .\dist\%app_name%\
 
 endlocal
