9a0f591922db85a3c581ef3f929695d0b20cadb1
yarvod <yar.vod.ole@gmail.com>
2023-12-11 18:25:19 +0300

    master - fix builds

--- build.bat
+++ build.bat
@@ -7,9 +7,6 @@ if "%1"=="" (
     set "app_name=%1"
 )
 
-pyInstaller main.py -n %app_name% --onedir --icon=".\assets\logo2.png" --noconsole --windowed -y
-mkdir .\dist\%app_name%\assets
-copy .\assets\* .\dist\%app_name%\assets\
-copy .\settings.ini .\dist\%app_name%\
+pyInstaller main.py -n %app_name% --onedir --icon=".\assets\logo2.png" --noconsole --windowed -y --add-data="assets:assets" --add-data="settings.ini:."
 
 endlocal
--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #! /bin/bash
 
-pyInstaller main.py -n SIS_manager --onedir --icon=assets/logo.png --noconsole -y
-
-mkdir ./dist/SIS_manager/assets
-cp ./assets/* ./dist/SIS_manager/assets/
-cp settings.ini ./dist/SIS_manager/
-
-mkdir ./dist/SIS_manager.app/Contents/MacOS/assets
-cp ./assets/* ./dist/SIS_manager.app/Contents/MacOS/assets/
-cp settings.ini ./dist/SIS_manager.app/Contents/MacOS/
+pyInstaller main.py -n SIS_manager --onedir --icon=assets/logo.png --noconsole -y --add-data="assets:assets" --add-data="settings.ini:."
