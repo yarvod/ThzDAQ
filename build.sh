0f5cee2cadc30a85b682e3620d676309edd89399
yarvod <yar.vod.ole@gmail.com>
2023-12-11 18:13:39 +0300

    master - fix build for macos

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-pyInstaller main.py -n SIS_manager --onedir --icon=./assets/logo.ico --noconsole -y
+pyInstaller main.py -n SIS_manager --onedir --icon=assets/logo.png --noconsole -y
+
 mkdir ./dist/SIS_manager/assets
 cp ./assets/* ./dist/SIS_manager/assets/
 cp settings.ini ./dist/SIS_manager/
+
+mkdir ./dist/SIS_manager.app/Contents/MacOS/assets
+cp ./assets/* ./dist/SIS_manager.app/Contents/MacOS/assets/
+cp settings.ini ./dist/SIS_manager.app/Contents/MacOS/
