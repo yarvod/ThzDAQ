2c210ca1804e0ce3ca8d711f9a960f2efbb90c48
Snake <yar.vod.ole@gmail.com>
2023-12-25 19:26:15 +0300

    dev - dfdfdf

--- build.bat
+++ build.bat
@@ -7,6 +7,8 @@ if "%1"=="" (
     set "app_name=%1"
 )
 
+copy .\dist\%app_name%\dumps\ .\dist\dumps\
+
 pyInstaller main.py -n %app_name% --onedir --icon=".\assets\logo2.png" --noconsole --windowed -y
 mkdir .\dist\%app_name%\assets
 copy .\assets\* .\dist\%app_name%\assets\
