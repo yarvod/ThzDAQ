2f6429b7347c077d9874c7257655a0c21109d22f
Snake <yar.vod.ole@gmail.com>
2024-04-01 15:14:09 +0300

    master - fix chopper read di 23

--- api/Chopper/chopper_ethernet.py
+++ api/Chopper/chopper_ethernet.py
@@ -1,10 +1,15 @@
+import logging
 import time
 from datetime import datetime
-from typing import Tuple
+from typing import Tuple, Union
 
 from pymodbus.client import ModbusTcpClient
+from pymodbus.exceptions import ModbusIOException
 
 from api.Chopper.chopper_sync import Chopper
+
+
+logger = logging.getLogger(__name__)
 
 
 class ChopperEthernet(Chopper):
@@ -32,11 +37,14 @@ class ChopperEthernet(Chopper):
             parity="N",
         )
 
-    def read_di23(self) -> Tuple[bool, bool]:
-        results = self.client.read_holding_registers(
+    def read_di23(self) -> Union[None, Tuple[bool, bool]]:
+        response = self.client.read_holding_registers(
             int(0x0179), count=1, slave=self.slave_address
-        ).registers
-        num = next((result for result in results), None)
+        )
+        if isinstance(response, ModbusIOException):
+            logger.error(f"{response}")
+            return None
+        num = next((result for result in response.registers), None)
         bits_str = "{0:08b}".format(num)
         return bits_str[-2] == "1", bits_str[-3] == "1"
 
@@ -48,5 +56,7 @@ if __name__ == "__main__":
         while 1:
             print(f"[{datetime.now()}]{chopper.read_di23()}")
             time.sleep(0.1)
+            chopper.path0()
+            time.sleep(0.3)
     except KeyboardInterrupt:
         chopper.client.close()
